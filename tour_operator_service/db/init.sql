84730ae795ecc9e7689d08dc24728bb054a088bb
JakubKoniuszewski <jakub.chrome@gmail.com>
2023-05-04 16:59:11 +0200

    cvs

--- tour_operator_service/db/init.sql
+++ tour_operator_service/db/init.sql
@@ -5,3 +5,5 @@ DROP USER if EXISTS tour_operator;
 CREATE USER "tour_operator" WITH password 'tour_operator';
 GRANT ALL ON database tour_operator_pg TO tour_operator;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON tables TO tour_operator;
+
+/* no tour_operator_readonly? */
