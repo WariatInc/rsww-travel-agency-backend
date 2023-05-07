84708cc65f8502d35328e0aaf5abe7eefbd6ce1d
Michał Meyer <mmey3r.dev@gmail.com>
2023-05-07 20:15:55 +0200

    Postgres user changes

--- api_gateway/db/init.sql
+++ api_gateway/db/init.sql
@@ -1,12 +1,7 @@
 DROP database if EXISTS api_gateway_pg;
 CREATE database api_gateway_pg;
 
-DROP USER if EXISTS api_gateway;
-CREATE USER "api_gateway" WITH password 'api_gateway';
-GRANT ALL ON database api_gateway_pg TO api_gateway;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON tables TO api_gateway;
-
-DROP USER if EXISTS api_gateway_readonly;
-CREATE USER "api_gateway_readonly" WITH password 'api_gateway';
-GRANT CONNECT ON DATABASE api_gateway_pg TO api_gateway_readonly;
-GRANT pg_read_all_data TO api_gateway_readonly;
+DROP USER if EXISTS "179919_api_gateway";
+CREATE USER "179919_api_gateway" WITH password 'api_gateway';
+GRANT ALL ON database api_gateway_pg TO "179919_api_gateway";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON tables TO "179919_api_gateway";
--- tour_operator_service/db/init.sql
+++ tour_operator_service/db/init.sql
@@ -1,9 +1,8 @@
 DROP database if EXISTS tour_operator_pg;
 CREATE database tour_operator_pg;
 
-DROP USER if EXISTS tour_operator;
-CREATE USER "tour_operator" WITH password 'tour_operator';
-GRANT ALL ON database tour_operator_pg TO tour_operator;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON tables TO tour_operator;
+DROP USER if EXISTS "179919_tour_operator";
+CREATE USER "179919_tour_operator" WITH password 'tour_operator';
+GRANT ALL ON database tour_operator_pg TO "179919_tour_operator";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON tables TO "179919_tour_operator";
 
-/* no tour_operator_readonly? */
